0a11b93051a78ab15eb4bff1095c90f56327b9b1
Federico <fgbarrientos93@gmail.com>
2022-01-22 18:18:23 -0300

    add hamburguer & overlay animation

--- app/js/script.js
+++ app/js/script.js
@@ -1,11 +1,20 @@
 const btnHamburger = document.querySelector('#btnHamburger')
+const  header = document.querySelector('.header')
+const overlay = document.querySelector('.overlay')
 
 btnHamburger.addEventListener('click', function () {
     console.log('open menu')
-    if(btnHamburger.classList.contains('open')){
-        btnHamburger.classList.remove('open')
+    if(header.classList.contains('open')){ //Close hamburguer menu
+        header.classList.remove('open')
+        overlay.classList.remove('fade-in')
+        overlay.classList.add('fade-out')
+
+
     }
-    else {
-        btnHamburger.classList.add('open')
+    else {// Open hamburguer menu
+        header.classList.add('open')
+        overlay.classList.add('fade-in')
+        overlay.classList.remove('fade-out')
+
     }
 })
